233da331863ea78dab152b46cc67e2d91ea99c23
Fabian Thomys <git@fthomys.me>
2025-04-14 11:28:24 +0200

    Add session handling in TestController with testAction method

--- mvc/lib/Controller/TestController.php
+++ mvc/lib/Controller/TestController.php
@@ -11,6 +11,7 @@ class TestController extends Controller
 
     public function indexAction()
     {
+        echo "<pre>";
         $flakegen = Snowflake::getInstance();
         $flake = $flakegen->generate();
         $decoded = $flakegen->decode($flake, 'Y-m-d H:i:s', 'Europe/Berlin');
@@ -21,6 +22,21 @@ class TestController extends Controller
         print_r($decoded);
 
         echo "Timestamp: " . $decoded['datetime'] . "\n";
+    }
+
+    public function testAction()
+    {
+        if (isset($_SESSION['user_id'])) {
+            echo "<pre>";
+            echo "Session is set.\n";
+            echo "User ID: " . $_SESSION['user_id'] . "\n";
+            echo "Session Created At: " . $_SESSION['session_created_at'] . "\n";
+            echo "Session ID: " . session_id() . "\n";
+
+            print_r($_SESSION);
+        } else {
+            echo "Session is not set.\n";
+        }
     }
 
 
